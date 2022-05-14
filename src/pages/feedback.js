b5b9df447dd28f1f6d697b7e7a2abd834dfeeaa6
NilsGke <n.j.goeke@gmail.com>
2022-05-15 00:52:22 +0200

    added feedback pre page

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -7,23 +7,5 @@ export default function FeedbackApp() {
     document.title = "Raumplan Feedback";
   }, []);
 
-  return (
-    <div id="feedbackContainer">
-      <form method="post">
-        <fieldset>
-          <legend>Umfrage</legend>
-          <h3>
-            Wie findest du es, dass die default Shortcuts umgeändert wurden? (zb{" "}
-            {MODIFIER_PREFIX} F -&gt; Benutzer Finden und nicht
-            Browser-suchfunktion){" "}
-          </h3>
-          <input type="radio" name="egal" id="r1" />
-          <label htmlFor="r1">Stört mich nicht</label>
-          <br />
-          <input type="radio" name="egal" id="r2" />
-          <label htmlFor="r2">Ist kacke</label>
-        </fieldset>
-      </form>
-    </div>
-  );
+  return <div id="feedbackContainer"></div>;
 }
